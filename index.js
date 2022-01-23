f30de0c5449ce9861072b42e9c839caf30282633
Klay Clarke <klayaclarke@gmail.com>
2022-01-22 21:55:23 -0500

    config express app - server listen - render homepage

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const ejsMate = require("ejs-mate");
+const path = require("path");
+
+const app = express();
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.listen(3000, () => {
+  console.log("Serving on Port 3000");
+});
