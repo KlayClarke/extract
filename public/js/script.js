4c6616b3fd5d7e681b878d150e621ede3708afa4
Klay Clarke <klayaclarke@gmail.com>
2022-01-23 01:37:50 -0500

    when folder icon clicked, it turns into an open folder icon

---
+++ public/js/script.js
@@ -0,0 +1,5 @@
+const folderButton = document.querySelector(".fa-folder");
+
+folderButton.addEventListener("click", function () {
+  folderButton.classList.replace("fa-folder", "fa-folder-open");
+});
