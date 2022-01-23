32edfeb1c1028d62c9446dd906398f1aa70479e5
Klay Clarke <klayaclarke@gmail.com>
2022-01-23 12:52:14 -0500

    add necessary packages - configure cloudinary in case I want to host files before sending to user

--- index.js
+++ index.js
@@ -2,63 +2,41 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+const fs = require("fs");
 const express = require("express");
 const ejsMate = require("ejs-mate");
+const cloudinary = require("cloudinary").v2;
+const cors = require("cors");
 const path = require("path");
-const axios = require("axios").default;
+// const axios = require("axios").default;
+const ytdl = require("ytdl-core");
+const ffmpeg = require("ffmpeg");
+// const { url } = require("inspector");
+// const { format } = require("path");
+// const { json } = require("express/lib/response");
 const app = express();
 
-const apiHost = process.env.YTMP3APIHOST;
-const apiKey = process.env.YTMP3APIKEY;
-
-console.log(apiHost);
-console.log(apiKey);
+app.use(cors());
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("/", (req, res) => {
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
+
+app.get("/", cors(), async (req, res) => {
   res.render("home");
 });
 
-app.post("/convert", (req, res) => {
-  const { id } = req.body;
-  const options = {
-    method: "GET",
-    url: "https://youtube-to-mp32.p.rapidapi.com/api/yt_to_mp3",
-    params: { video_id: id },
-    headers: {
-      "x-rapidapi-host": process.env.YTMP3APIHOST,
-      "x-rapidapi-key": process.env.YTMP3APIKEY,
-    },
-  };
-
-  axios
-    .request(options)
-    .then(function (response) {
-      const { data, status } = response;
-      if (status === 200) {
-        return res.render("response", {
-          success: true,
-          message: "Audio Successfully Extracted",
-          title: data.Title,
-          link: data.Download_url,
-        });
-      } else {
-        return res.render("response", {
-          success: false,
-          message: "Audio Extraction Error - Please Try Again",
-        });
-      }
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-});
+app.post("/convert", cors(), (req, res) => {});
 
 app.listen(3000, () => {
   console.log("Serving on Port 3000");
