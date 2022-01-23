bdf03857f715288aa3d188780be43fc1c9f4445b
Klay Clarke <klayaclarke@gmail.com>
2022-01-23 14:37:16 -0500

    aws amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "extract83310e7f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "musicfiles": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
